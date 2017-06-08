e35c054d7883fe4cd8eef2efe22d3d2301022a13
sandro.coelho <sandroacoelho@gmail.com>
2017-06-08 00:24:33 -0300

    java.lang.StringIndexOutOfBoundsException: String index out of range: 4

--- core/src/main/scala/org/dbpedia/spotlight/model/OntologyType.scala
+++ core/src/main/scala/org/dbpedia/spotlight/model/OntologyType.scala
@@ -72,8 +72,8 @@ class DBpediaType(var name : String) extends OntologyType {
         name.equalsIgnoreCase(that.name)
     }
 
-    override def getFullUri =  if (name.substring(0,4).equalsIgnoreCase("http")) name else DBpediaType.DBPEDIA_ONTOLOGY_PREFIX + name
-    override def typeID = if (name.substring(0,4).equalsIgnoreCase("http")) name else "DBpedia:".concat(name)
+    override def getFullUri =  if (name.length >= 4 && name.substring(0,4).equalsIgnoreCase("http")) name else DBpediaType.DBPEDIA_ONTOLOGY_PREFIX + name
+    override def typeID = if (name.length >= 4 && name.substring(0,4).equalsIgnoreCase("http")) name else "DBpedia:".concat(name)
 
 }
 
