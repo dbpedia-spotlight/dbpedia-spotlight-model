ea93b19bb9a46cde869484a20d4a44377e1e2ae1
sandro.coelho <sandroacoelho@gmail.com>
2017-06-28 00:36:14 -0300

    Fixing Index process trying to find a Stemmer for Chinese  - Issue #11

--- index/src/main/scala/org/dbpedia/spotlight/db/CreateSpotlightModel.scala
+++ index/src/main/scala/org/dbpedia/spotlight/db/CreateSpotlightModel.scala
@@ -47,7 +47,7 @@ object CreateSpotlightModel {
         new File(args(2)), // output folder
         if (args(3) equals "None") None else Some(new File(args(3))), // openNLP
         new File(args(4)), // stopwords
-        if (args(5) equals "None") new Stemmer() else new SnowballStemmer(args(5)) // stemmer
+        if ((args(5) equals "None") || (args (5) equals "NoneStemmer")) new Stemmer() else new SnowballStemmer(args(5)) // stemmer
         )
     } catch {
       case e: Exception => {
