9f777351d235a89d5978066ab36c8501f11b0516
Skunnyk <skunnyk@alteroot.org>
2017-10-27 18:09:25 +0200

    Migrate away from log4j to SpotlightLog
    - some log4j were still used and worked due to dependencie chain ...
    - Replace it with SpotlightLog
    - Still a WIP of course

--- core/src/main/java/org/dbpedia/spotlight/sparql/SparqlQueryExecuter.java
+++ core/src/main/java/org/dbpedia/spotlight/sparql/SparqlQueryExecuter.java
@@ -24,7 +24,7 @@ import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.params.HttpMethodParams;
-import org.apache.log4j.Logger;
+import org.dbpedia.spotlight.log.SpotlightLog;
 import org.dbpedia.spotlight.exceptions.OutputException;
 import org.dbpedia.spotlight.exceptions.SparqlExecutionException;
 import org.dbpedia.spotlight.model.DBpediaResource;
@@ -51,7 +51,6 @@ import java.util.List;
  */
 public class SparqlQueryExecuter {
 
-	private final static Logger LOG = Logger.getLogger(SparqlQueryExecuter.class);
 
        // Create an instance of HttpClient.
     private static HttpClient client = new HttpClient();
@@ -76,10 +75,10 @@ public class SparqlQueryExecuter {
 
 	public List<DBpediaResource> query(String query) throws IOException, OutputException, SparqlExecutionException {
         if (query==null) return new ArrayList<DBpediaResource>();
-		LOG.debug("--SPARQL QUERY: " + query.replace("\n", " "));
+		//LOG.debug("--SPARQL QUERY: " + query.replace("\n", " "));
+		//SpotlightLog.debug(SparqlQueryExecuter.class, "--SPARQL QUERY: %s" , query.replace("\n", " ")); 
 
 		URL url = getUrl(query);
-		//LOG.trace(url);
 
 		//FIXME Do some test with the returned results to see if there actually are results.
         List<DBpediaResource> uris = null;
@@ -91,7 +90,7 @@ public class SparqlQueryExecuter {
         } catch (JSONException e) {
             throw new OutputException(e+response);
         }
-        LOG.debug(String.format("-- %s found.", uris.size()));
+	//SpotlightLog.debug(SparqlQueryExecuter.class, "-- %s found.", uris.size());
         return uris;
 	}
 
@@ -119,7 +118,7 @@ public class SparqlQueryExecuter {
             int statusCode = client.executeMethod(method);
 
             if (statusCode != HttpStatus.SC_OK) {
-                LOG.error("SparqlQuery failed: " + method.getStatusLine());
+		//SpotlightLog.debug(SparqlQueryExecuter.class,"SparqlQuery failed: %s", method.getStatusLine());
                 throw new SparqlExecutionException(String.format("%s (%s). %s",
                                                                  method.getStatusLine(),
                                                                  method.getURI(),
